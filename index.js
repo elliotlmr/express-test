f8d1ae2e150081cb21e736d1b99b9253d62d19e2
HAL13000 <haru.smile.13@gmail.com>
2023-10-13 19:11:21 +0200

    commit by Haru

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = 6000;
 require("dotenv").config();
+const CryptoJS = require("crypto-js");
+const User = require("./src/user");
 
+app.use(express.json());
 // Connect to MongoDB
 try {
   mongoose.connect(process.env.MONGODB_URL);
@@ -11,6 +14,21 @@ try {
 } catch (err) {
   console.log(err);
 }
+
+// Register API
+app.post("/register", async (req, res) => {
+  const password = req.body.password;
+
+  try {
+    // secret password
+    req.body.password = CryptoJS.AES.encrypt(
+      "password",
+      process.env.SECRET_KEY
+    );
+    // new user
+    const user = await User.create(req.body);
+  } catch {}
+});
 
 app.listen(PORT, () => {
   console.log("local server is running");
