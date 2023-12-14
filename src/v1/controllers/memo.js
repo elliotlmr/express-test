b6535e0024cc0bf2e342fb3fa0db9da87cdc3423
HAL13000 <haru.smile.13@gmail.com>
2023-12-14 17:26:46 +0100

    commit by Haru

---
+++ src/v1/controllers/memo.js
@@ -0,0 +1,14 @@
+const Memo = require("../modules/memo");
+
+exports.create = async (req, res) => {
+  try {
+    const memoCount = await Memo.find().count();
+    const memo = await Memo.create({
+      user: req.user._id,
+      position: memoCount > 0 ? memoCount : 0,
+    });
+    res.status(201).json(memo);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
