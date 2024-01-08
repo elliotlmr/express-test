feef9055e015de5fe9d2c464f0eca1b2866d25a6
HAL13000 <haru.smile.13@gmail.com>
2024-01-08 18:16:17 +0100

    commit by Haru

--- src/v1/controllers/memo.js
+++ src/v1/controllers/memo.js
@@ -23,9 +23,26 @@ exports.getAll = async (req, res) => {
 exports.getOne = async (req, res) => {
   const { memoId } = req.params;
   try {
-    const memo = Memo.findOne({ user: req.user._id, _id: memoId });
+    const memo = await Memo.findOne({ user: req.user._id, _id: memoId });
     if (!memo) return res.status(404).json("Memo doesn't exist");
     res.status(200).json(memo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+exports.update = async (req, res) => {
+  const { memoId } = req.params;
+  const { title, description } = req.body;
+  try {
+    if (title === "") req.body.title = "No Title";
+    if (description === "") req.body.description = "No Description";
+    const memo = await Memo.findOne({ user: req.user._id, _id: memoId });
+    if (!memo) return res.status(404).json("Memo doesn't exist");
+
+    const updatedMemo = await Memo.findByIdAndUpdate(memoId, {
+      $set: req.body,
+    });
+    res.status(200).json(updatedMemo);
   } catch (err) {
     res.status(500).json(err);
   }
